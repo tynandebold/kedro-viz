4b0400c90acc5fca65158948aee32957bb465216
Tynan DeBold <thdebold@gmail.com>
2021-10-27 16:57:35 +0100

    moving GraphQL provider to wrapper component

--- src/components/experiment-wrapper/index.js
+++ src/components/experiment-wrapper/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Sidebar from '../experiment-tracking/sidebar';
 import Details from '../experiment-tracking/details';
-import { Provider } from '../provider/provider';
 
 /**
  * Main experiment tracking page container. Handles showing/hiding the sidebar
@@ -11,14 +10,12 @@ import { Provider } from '../provider/provider';
  */
 const ExperimentWrapper = () => (
   <>
-    <Provider useMocks={true}>
-      <Sidebar />
-      <Switch>
-        <Route path={['/runsList/:id', '/runsList']}>
-          <Details />
-        </Route>
-      </Switch>
-    </Provider>
+    <Sidebar />
+    <Switch>
+      <Route path={['/runsList/:id', '/runsList']}>
+        <Details />
+      </Route>
+    </Switch>
   </>
 );
 
