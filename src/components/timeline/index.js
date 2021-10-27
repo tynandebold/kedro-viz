8f47c2f81fd23740b23d5bdbc0b1cc92579ccfa8
Huong Nguyen <huongg1409@gmail.com>
2021-10-27 16:08:41 +0100

    styling for timeline component

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -3,13 +3,11 @@ import { connect } from 'react-redux';
 
 import './timeline.css';
 
-const width = 1500;
 const height = 300;
-
-const DURATION = 700;
-const TRANSITION_WAIT = 200;
+const leftArea = 480;
 
 export const Timeline = ({ visible }) => {
+  const width = window.innerWidth - leftArea;
   const transformStyle = {
     transform: `translate(calc(-100% + ${width}px), -100%)`,
   };
@@ -20,31 +18,20 @@ export const Timeline = ({ visible }) => {
       style={visible ? transformStyle : {}}
     >
       <div className="pipeline-timeline">
-        {/* ref={this.containerRef}> */}
         <svg
           id="pipeline-timeline-graph"
           className="pipeline-timeline__graph"
           width={width}
           height={height}
           viewBox={`0 0 ${width} ${height}`}
-        >
-          <rect
-            className="pipeline-timeline__viewport"
-            // ref={this.viewportRef}
-          />
-        </svg>
+        />
       </div>
     </div>
   );
 };
 
-export const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = (state) => ({
   visible: state.visible.timeline,
-  ...ownProps,
 });
 
-export const mapDispatchToProps = (dispatch, ownProps) => ({
-  ...ownProps,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Timeline);
+export default connect(mapStateToProps)(Timeline);
